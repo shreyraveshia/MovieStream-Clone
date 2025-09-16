8196acc7b11050c8f8298127b78bbbe0f43a7266
shreyraveshia <shreyraveshia07@gmail.com>
2025-09-17 02:03:29 +0530

    Login Page of Netflix on progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <React.StrictMode>
+    <BrowserRouter>  
+
+      <App />
+
+    </BrowserRouter>
+{/* // BrowserRouter will help us to use the routing in our app.
+// Now we will get the support of this react router in this app component. */}
+  </React.StrictMode>,
 )
