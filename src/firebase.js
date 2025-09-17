ac3df7b7f33ca8c8ae7181c5a14826c2761d3685
shreyraveshia <shreyraveshia07@gmail.com>
2025-09-18 04:07:53 +0530

    Login and Registration feature using Firebase- 70% done

---
+++ src/firebase.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_4yhVB5APKsmPXk54udCaD4yMd-XXqFs",
+  authDomain: "netflix-clone-c3f02.firebaseapp.com",
+  projectId: "netflix-clone-c3f02",
+  storageBucket: "netflix-clone-c3f02.firebasestorage.app",
+  messagingSenderId: "201027823497",
+  appId: "1:201027823497:web:4b8e2fddbad3e7d8d8e358"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// now we will get support of firebase in our project
+
+// So after initializing this firebase, we will initialize the firebase authentication in our project.
+const auth= getAuth(app);
+// after this we have to configure the FireStore also for the database.
+
+const db= getFirestore(app);
+
+// after this we will create functions for signup, login and logout in our project.
+
+// first we will create user-signup functions.
+
+const signup = async (name, email, password)=>{
+    try{
+        const res = await createUserWithEmailAndPassword(auth, email, password); // this function will create user with email and password. 
+        // we will get the created user in this res.
+        const user= res.user;
+        //  And from this res we will get user details that is stored in this '.user' property.
+        // so now we have created the user in our firebase
+        // next we will store the user information in our firestore database.
+
+        await addDoc(collection(db, "users"),{
+            // in this object we are defining the data that will be stored in this user collection.
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+            // so we will store these 4 things in the database.
+        });
+    } catch(error){
+        console.log(error);
+        alert(error);
+    }
+}
+// so we have created the user sign up function, and after this we will create login function.
+
+const login = async (email, password) =>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        // using this line user would be logged in our website.
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout =()=>{
+    signOut(auth);
+}
+
+export{ auth, db, login, signup, logout};
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 import { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import {login, signup} from '../../firebase'
 
 const Login = () => {
 
-   const [signState, setSignState] = useState("Sign In")
+   const [signState, setSignState] = useState("Sign In");
 
+// now we will create the state variable that will store the form data where input fields are like name, email, password.
+// so we will store these forms data in the state variable.
+
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   // after witing these, we have to connect these State variable with the input fields.
+   // so that when we add data in the input fields, that will be saved in these states variable.
+
+  // now we have to create function for user-authentication.
+   const user_auth = async (event)=>{
+    event.preventDefault();                        // whenever we submit the form we will get the event here. 
+    if(signState==="Sign In"){                 // Using this event we will add prevent default,                                             
+      await login(email, password);            // so it will not reload the web page whenever we submit the form.
+    } else {
+      await signup(name, email, password)
+
+    }
+   } 
+   // next we have to connect this user_auth function with this submit button
 
 
   return (
@@ -16,13 +37,20 @@ const Login = () => {
       <div className="login-form">
 
         <h1> {signState} </h1>
+
         <form>
-          {signState==="Sign Up"?<input type="text" placeholder='Your Name' />: <></>}
+          {signState==="Sign Up"? <input value={name} onChange={ (e) => { setName(e.target.value)} } 
+          type="text" placeholder='Your Name' />: <></>}
 
-          <input type="email" placeholder='email' />
-          <input type="password" placeholder='Password' />
-          <button>{signState}</button>
+          <input value={email} onChange={ (e) => { setEmail(e.target.value)} }   
+          type="email" placeholder='email' />
 
+          <input value={password} onChange={ (e) => {setPassword(e.target.value)} }
+           type="password" placeholder='Password' />
+
+          <button onClick={user_auth} type='submit' > {signState} </button> 
+
+ {/* So now whenever we will enter any data into our input field, those values will be stored in the state variable. */}
           <div className="form-help">
 
             <div className="remember">
