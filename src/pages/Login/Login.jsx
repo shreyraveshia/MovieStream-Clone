be8b86a1ffa262a89f57e8355688d86dbd354991
shreyraveshia <shreyraveshia07@gmail.com>
2025-09-18 12:28:52 +0530

    Created Login animation in Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 import {login, signup} from '../../firebase'
+import netflix_spinner from '../../assets/netflix_spinner.gif'  // we will use this gif when we click on sign-in or sign-up, then login image will be displayed, and once we are logged in that will be removed.
+
+
 
 const Login = () => {
 
@@ -17,20 +20,31 @@ const Login = () => {
    // after witing these, we have to connect these State variable with the input fields.
    // so that when we add data in the input fields, that will be saved in these states variable.
 
+  const [loading, setLoading] = useState(false);   // whenever we will click login or sign up button, this this loading state will be true.
+
+
   // now we have to create function for user-authentication.
    const user_auth = async (event)=>{
     event.preventDefault();                        // whenever we submit the form we will get the event here. 
+   
+    setLoading(true);
+    
     if(signState==="Sign In"){                 // Using this event we will add prevent default,                                             
       await login(email, password);            // so it will not reload the web page whenever we submit the form.
     } else {
-      await signup(name, email, password)
-
+      await signup(name, email, password)      
     }
+    setLoading(false);
    } 
    // next we have to connect this user_auth function with this submit button
 
 
   return (
+
+    loading? <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>:
+
     <div className='login'>
 
       <img src={logo} className='login-logo' alt="" />
